185a2a74e3a3b5d56a28626aa636ebe2e767d14b
jjeongah <j_eah@naver.com>
2023-10-24 00:44:55 +0900

    Publications page

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function showAbout() {
+    window.location.href = "https://jjeongah.github.io";
+}
+
+function showPublications() {
+    window.location.href = "https://jjeongah.github.io/publications";
+}
+
+function showProjects() {
+    window.location.href = "https://jjeongah.github.io/projects";
+}
+
+function showCV() {
+    window.location.href = "https://drive.google.com/file/d/1ViAhyEpV-rP8_7GoUuzFvA7GLaSN0T8-/view?usp=share_link";
+}
